3813d51538c09702e277485c72b310e7b95d672c
abrenzink <a.brenzink@gmail.com>
2026-01-12 22:00:31 -0300

    feat: adding views

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -6,14 +6,23 @@ namespace MvcMovie.Controllers;
 public class HelloWorldController : Controller
 {
     // GET: /HelloWorld/
-    public string Index()
+    public IActionResult Index()
     {
-        return "This is my default action...";
+        return View();
     }
-    // 
     // GET: /HelloWorld/Welcome/ 
-    public string Welcome()
+    // Requires using System.Text.Encodings.Web;
+    public IActionResult Welcome(string name, int numTimes = 1)
     {
-        return "This is the Welcome action method...";
+        /*
+        The ViewData dictionary is a dynamic object, which means any 
+        type can be used. The ViewData object has no defined properties 
+        until something is added. The MVC model binding system automatically 
+        maps the named parameters name and numTimes from the query string to 
+        parameters in the method. The complete HelloWorldController
+        */
+        ViewData["Message"] = "Hello " + name;
+        ViewData["NumTimes"] = numTimes;
+        return View();
     }
 }
