717a228e09169fb31926878e5e4f5f8d63206910
abrenzink <a.brenzink@gmail.com>
2026-01-12 22:47:48 -0300

    feat: adding model, migration, DB, and seeding DB

--- obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75f9ecb744f7cd9f4c897729378eaf0c2c5306f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3813d51538c09702e277485c72b310e7b95d672c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
